868923cc5a972ef9a6a22f1cb33df83afdc0c705
msctrl0 <940356316@qq.com>
2016-08-16 15:11:19 +0800

    fullscreenToggle

--- fullscreenToggle/js/fullscreentoggle.js
+++ fullscreenToggle/js/fullscreentoggle.js
@@ -1,20 +1,30 @@
 
 Zepto(function($){
-	var lis=document.getElementsByTagName('li');
-	$('ul>li').css({"height":$( window).height()});
-
-	/*	alert(lis.length);*/
-
-	//禁止默认滑动事件
-	document.addEventListener('touchmove', function (event) {
-		event.preventDefault();
-	}, false);
-
-	$('ul>li').swipeUp(function(){
-		$('.ul1').addClass('up');
-
-	})
-	$('ul>li').swipeDown(function(){
-		alert("a");
-	})
-})
+	var doc=$('.toggle');
+	function fullscreenToggle(doc,times){
+		doc.css({"-webkit-transition":"transform"+" " + times});
+		var children=doc.children();
+		children.css({"height":$( window).height()});
+		var len=children.length;
+		var now=1;
+		//禁止默认滑动事件
+		document.addEventListener('touchmove', function (event) {
+			event.preventDefault();
+		}, false);	
+		//上滑
+		doc.swipeUp(function(){
+			if(0<now&&now<len){
+				$(this).css({"transform":"translate(0px,-"+now*$( window).height()+"px)"});
+				now++;
+			}
+		})
+		//下滑
+		doc.swipeDown(function(){
+			if(1<now&&now<len+1){
+				$(this).css({"transform":"translate(0px,-"+(now-2)*$( window).height()+"px)"});
+				now--;
+			}
+		})	
+	}
+	fullscreenToggle(doc,"0.5s");	
+}) 
