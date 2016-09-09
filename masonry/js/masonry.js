da494eccfe32461f7507eaa9dd693f69f34f43a3
msctrl0 <940356316@qq.com>
2016-09-09 17:22:51 +0800

    masonry

---
+++ masonry/js/masonry.js
@@ -0,0 +1,32 @@
+(function(){
+	var u=document.getElementsByTagName("ul");
+	/*console.log(u[0].children[0].offsetHeight);
+	console.log(u[0].offsetHeight);*/
+	var heights=[0,0,0,0];
+	var min=0;
+	var getMin=function(heights){
+
+		/*for (var i = 0; i < heights.length; i++) {
+			heights[i]
+		}*/
+		/*heights[0]=1;
+		return heights[0];*/
+		var i=0;
+		function abc(heights) {
+			// body...
+			
+			/*while(i<4){
+				i++;
+				return heights[i-1]<heights[i]?heights[i-1]:heights[i];
+			}*/
+			
+		}
+		abc(heights);
+	};
+
+
+	getMin(heights);
+	/*for (var i = 0; i < heights.length; i++) {
+		console.log(heights[i])
+	}*/
+})(); 
