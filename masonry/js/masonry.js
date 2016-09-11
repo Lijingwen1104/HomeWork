875b6648fd540c748dfea6ec57b9dfbff514a7be
MsCtrl0 <940356316@qq.com>
2016-09-11 22:52:56 +0800

    9.11

--- masonry/js/masonry.js
+++ masonry/js/masonry.js
@@ -1,32 +1,20 @@
 (function(){
 	var u=document.getElementsByTagName("ul");
-	/*console.log(u[0].children[0].offsetHeight);
-	console.log(u[0].offsetHeight);*/
-	var heights=[0,0,0,0];
-	var min=0;
-	var getMin=function(heights){
+	console.log(u[0].children[0].offsetHeight);
+	console.log(u[0].offsetHeight);
+	var c
+	//当前各列高度初始化
+	var heights;
 
-		/*for (var i = 0; i < heights.length; i++) {
-			heights[i]
-		}*/
-		/*heights[0]=1;
-		return heights[0];*/
-		var i=0;
-		function abc(heights) {
-			// body...
-			
-			/*while(i<4){
-				i++;
-				return heights[i-1]<heights[i]?heights[i-1]:heights[i];
-			}*/
-			
-		}
-		abc(heights);
-	};
-
-
-	getMin(heights);
-	/*for (var i = 0; i < heights.length; i++) {
-		console.log(heights[i])
-	}*/
+	//获取最小值和最小值所在的位置
+	function getMin(attr){
+		return (function abc(i){
+			if(i<=0){
+				return {num:0,val:attr[0]};
+			}else{
+				return attr[i]<abc(i-1).val?{num:(i),val:attr[i]}:abc(i-1);
+			}
+		})(attr.length-1);
+	}
+	
 })(); 
